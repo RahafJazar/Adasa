65d0b181e7feac74a7e9063c7418a1a61f734203
Rahaf Jazar <rahafjazar73@gmail.com>
2026-07-29 16:29:31 +0300

    adasa_V3_29/7/2026

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'about', component: AboutUsComponent, title: "Route | about-us" },
+    { path: 'blog', component: BlogComponent, title: "Route | blog " },
+    { path: 'home', component: HomeComponent, title: 'Route | home' }
+
+];
