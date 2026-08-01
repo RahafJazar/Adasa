e3739ddfc1df92743b5720b1ae7146d06e759d5a
Rahaf Jazar <rahafjazar73@gmail.com>
2026-08-01 11:35:03 +0300

    adasa_v4_1/8/2026

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 import { RouterLink, RouterLinkActive } from "@angular/router";
+
 
 @Component({
   selector: 'app-navbar',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
+  standalone: true,
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
